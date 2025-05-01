ac6669da07aab52d83a304d81ded664d9c25b2ec
clz2025-red <clz2025.red@gmail.com>
2025-05-01 14:03:52 +0900

    2025-05-01

--- src/com/javaex/ex14/GoodsApp.java
+++ src/com/javaex/ex14/GoodsApp.java
@@ -2,25 +2,20 @@ package com.javaex.ex14;
 
 public class GoodsApp {
 
-	//static 조사--> 스택영역에 올려놓는다
-	
+	//static 조사--> 스태틱영역에 올려놓는다
+    //스태틱 메모리에서 main() 를 실행한다 	
+
 	public static void main(String[] args) {
-		
 		
 		Goods camera = new Goods();
 		camera.setName("니콘");
 		camera.setPrice(40000);
-		
-		
-		
 		System.out.println(camera.getCount());
 		
 		
 		Goods cup = new Goods("머그컵", 2000);	
-		
-		
-		
 		System.out.println(cup.getCount());
+		
 		
 		
 		
