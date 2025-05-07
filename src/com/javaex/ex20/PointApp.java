022ee9ae0d563814d048cd85ae1c48946792d567
clz2025-red <clz2025.red@gmail.com>
2025-05-07 11:16:16 +0900

    2025-05-07

--- src/com/javaex/ex20/PointApp.java
+++ src/com/javaex/ex20/PointApp.java
@@ -2,9 +2,20 @@ package com.javaex.ex20;
 
 public class PointApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {	
 
+		Point p = new Point();
+		//System.out.println(p.toString());
+		
+		
+		ColorPoint cp = new ColorPoint(5, 3, "red");
+		System.out.println(cp.toString());
+		
+		cp.draw();
+		
+		//Point(2)
+		//ColorPoint(3)
+		
 	}
 
 }
